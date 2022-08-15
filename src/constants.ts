0f31351a7fec83b7b14ce40baa9b646769cfad7a
Nataliie Utochkina <unatart@outlook.com>
2022-08-16 01:28:42 +0200

    updated json

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const TREE_ELEMENT_Y_OFFSET = 30;
 export const TREE_ELEMENT_X_OFFSET = 50;
-export const BASE_TREE_LINK = "/data_samples/sample_big.json";
+export const BASE_TREE_LINK = "/data_samples/sample_task.json";
 export const BASE_PAGE_SIZE = 35;
 export const BASE_TOLERANCE = 10;
